ee1834f91c823940d601768200e624bab6b43405
Hellshi <hcslimaa@gmail.com>
2021-06-20 12:12:00 -0300

    Created some boilerplates and got some music date
    Those songs don't belong to me. Please don'r prosecute me

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles/app.scss";
+import Player from "./components/Player";
+import Song from "./components/Song";
+import data from "./util";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Song></Song>
+      <Player></Player>
     </div>
   );
 }
