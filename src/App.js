d8e5dc555636c7e97a268ba4c7b2f7dd25771741
Hellshi <hcslimaa@gmail.com>
2021-06-20 13:37:51 -0300

    Added functionalities
    Play button now works,
    The "end song" and "duration" now are dinamic for the song

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ import data from "./util";
 
 function App() {
   const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
-      <Song></Song>
-      <Player></Player>
+      <Song currentSong={currentSong}></Song>
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      ></Player>
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,29 +1,77 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
   faAngleLeft,
   faAngleRight,
+  faPause,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+  //State player
+  const [playerButton, setPlayerButton] = useState(faPlay);
+  //Time info
+  const [songInfo, setSongInfo] = useState({
+    currentTime: null,
+    duration: null,
+  });
+  //Ref
+  const audioRef = useRef(null);
+
+  //PlaySong
+  const handlePlay = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(!isPlaying);
+      setPlayerButton(faPlay);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying);
+      setPlayerButton(faPause);
+    }
+  };
+
+  //Show time
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime: current, duration: duration });
+  };
+
+  //Format song time
+  function getTime(time) {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  }
+
   return (
     <div className="player">
       <div className="time-control">
-        <p>Start time</p>
+        <p>{getTime(songInfo.currentTime)}</p>
         <input type="range" />
-        <p>End time</p>
+        <p>{getTime(songInfo.duration)}</p>
       </div>
 
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-        <FontAwesomeIcon className="icon" size="2x" icon={faPlay} />
+        <FontAwesomeIcon
+          className="icon"
+          onClick={handlePlay}
+          size="2x"
+          icon={playerButton}
+        />
         <FontAwesomeIcon
           className="skip-forward"
           size="2x"
           icon={faAngleRight}
         />
       </div>
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
     </div>
   );
 };
